816ddd070740fb0d36e2ec86f2e2a17028c819a6
Benoît Hubert <benoithubert@gmail.com>
2021-12-03 18:57:58 +0100

    run autopep8

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -5,51 +5,53 @@ import mysql.connector
 
 config = dotenv_values(".env")
 connection_params = {
-  'host': config['DB_HOST'],
-  'user': config['DB_USER'],
-  'password': config['DB_PASS'],
-  'database': config['DB_NAME']
+    'host': config['DB_HOST'],
+    'user': config['DB_USER'],
+    'password': config['DB_PASS'],
+    'database': config['DB_NAME']
 }
 db = mysql.connector.connect(**connection_params)
 
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route("/")
 def hello_world():
-    return { "message": "Hello World" }
+    return {"message": "Hello World"}
+
 
 @app.route("/api/tasks", methods=["GET", "POST"])
 def tasks():
     error = None
     if request.method == 'POST':
-      query = "INSERT INTO `task` (`label`) VALUES (%s)"
-      label = request.form['label']
-      params = (label,)
-      with db.cursor() as c:
-          try:
-            c.execute(query, params)
-            db.commit()
-            id = c.lastrowid
-            return {
-              "id": id,
-              "label": label
-            }
-          except mysql.connector.errors.ProgrammingError as err:
-            print(err)
-            return { "status": "err" }
+        query = "INSERT INTO `task` (`label`) VALUES (%s)"
+        label = request.form['label']
+        params = (label,)
+        with db.cursor() as c:
+            try:
+                c.execute(query, params)
+                db.commit()
+                id = c.lastrowid
+                return {
+                    "id": id,
+                    "label": label
+                }
+            except mysql.connector.errors.ProgrammingError as err:
+                print(err)
+                return {"status": "err"}
     else:
-      query = "SELECT * FROM `task`"
-      with db.cursor() as c:
-          try:
-            c.execute(query)
-            rv = c.fetchall()
-            row_headers=[x[0] for x in c.description]
-            json_data = []
-            for result in rv:
-                json_data.append(dict(zip(row_headers,result)))
-            return jsonify(json_data)
-          except mysql.connector.errors.ProgrammingError as err:
-            print(err)
-            return { "status": "err" }
-      return jsonify([task.to_json() for task in tasks])
+        query = "SELECT * FROM `task`"
+        with db.cursor() as c:
+            try:
+                c.execute(query)
+                rv = c.fetchall()
+                row_headers = [x[0] for x in c.description]
+                json_data = []
+                for result in rv:
+                    json_data.append(dict(zip(row_headers, result)))
+                return jsonify(json_data)
+            except mysql.connector.errors.ProgrammingError as err:
+                print(err)
+                return {"status": "err"}
+        return jsonify([task.to_json() for task in tasks])
